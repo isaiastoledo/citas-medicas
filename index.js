ad284e44480ef553bfa8692eb45ceac4e45bdc35
isaias toledo <isaias.toledo.v@gmail.com>
2024-05-06 19:42:27 -0400

    modificando index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import routes from './routes/routes.js';
-
+import router from './routes/router.js';
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use('/', routes);
+//Routes
+app.use('/', router);
 
 app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
